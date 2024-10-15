a01e9fadac81c5c45eeaf4c5967748533eb0b134
MarnelleApat <marnelle.apat@bible.org.sg>
2024-10-15 11:56:14 +0800

    fixing the fileand route

--- resources/views/partials/programs-menu.blade.php
+++ resources/views/partials/programs-menu.blade.php
@@ -5,8 +5,10 @@
             <li class="">
                 <a wire:navigate 
                     href="{{route('admin.programme-item', $item->slug)}}" 
-                    class="{{ Route::current()->getName() === 'admin.programme-item' && Route::current()->parameters()['program_slug'] === $item->slug ? 'bg-graydark dark:bg-meta-4' : '' }} capitalize pl-10 py-2 group relative flex items-center gap-1 rounded-none font-medium text-white hover:bg-graydark dark:hover:bg-meta-4 duration-300 ease-in-out">
+                    class="{{ Route::current()->getName() === 'admin.'.$item->slug.'-profile' || (Route::current()->getName() === 'admin.programme-item' && Route::current()->parameters()['program_slug'] === $item->slug) ? 'bg-graydark dark:bg-meta-4' : '' }} capitalize pl-10 py-2 group relative flex items-center gap-1 rounded-none font-medium text-white hover:bg-graydark dark:hover:bg-meta-4 duration-300 ease-in-out">
                     {{ $item->name }}
+                    {{-- {{ Route::current()->getName() }} --}}
+                    {{-- admin.{{$item->slug}}-profile --}}
                 </a>
             </li> 
         @endforeach
