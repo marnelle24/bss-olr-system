813661a0b783322517429682d2b2bb11496d781c
MarnelleApat <marnelle.apat@bible.org.sg>
2024-11-04 10:17:26 +0800

    add registration model & migration

---
+++ app/Models/Registrant.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Registrant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'regCode',
+        'programCode',
+        'user_id',
+        'nric',
+        'title',
+        'firstName',
+        'lastName',
+        'address',
+        'city',
+        'postalCode',
+        'email',
+        'contactNumber',
+        'extraFields',
+        'paymentStatus',
+        'paymentGateway',
+        'paymentReferenceNo',
+        'regStatus',
+        'emailStatus',
+        'soft_delete',
+    ];
+}
---
+++ database/migrations/2024_11_01_073340_create_registrants_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('registrants', function (Blueprint $table) {
+            $table->id();
+            $table->string('regCode')->unique();
+            $table->string('programCode');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('nric')->nullable();
+            $table->string('title')->nullable();
+            $table->string('firstName')->nullable();
+            $table->string('lastName')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('postalCode')->nullable();
+            $table->string('email')->nullable();
+            $table->string('contactNumber')->nullable();
+            $table->text('extraFields')->nullable();
+            $table->boolean('paymentStatus')->default(0);
+            $table->string('paymentGateway')->nullable(); // paynow|card
+            $table->string('paymentReferenceNo')->nullable();
+            $table->string('regStatus')->nullable();
+            $table->boolean('emailStatus')->default(0);
+            $table->boolean('soft_delete')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('registrants');
+    }
+};
